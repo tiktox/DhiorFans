35cd2876187a3b6b14abf19220b08fd41a23b753
tiktox <sp9979182@gmail.com>
2025-10-17 06:28:18 -1200

    p

--- components/BasicEditor.tsx
+++ components/BasicEditor.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useCallback } from 'react';
+import { useState, useRef, useCallback, useEffect } from 'react';
 import { auth } from '../lib/firebase';
 import { getUserData, saveUserData } from '../lib/userService';
 import { createPost } from '../lib/postService';
